4439779c5c97abdd665e15c486bd197fcb27568f
Ryan Rubin <ryan.rubin@cognizant.com>
2024-04-06 04:15:07 +0800

    tweak movement

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    const float Deceleration = 20;
+    const float JumpVel = 10;
+    const float Speed = 10;
+
     Rigidbody rb;
 
     void Start()
@@ -16,19 +20,34 @@ public class PlayerScript : MonoBehaviour
         var vel = rb.velocity;
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            vel.x = -10;
+            vel.x = -Speed;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            vel.x = 10;
+            vel.x = Speed;
         }
         else
         {
-            vel.x = 0;
+            if (vel.x < 0)
+            {
+                vel.x += Deceleration * Time.deltaTime;
+                if (vel.x > 0)
+                {
+                    vel.x = 0;
+                }
+            }
+            else if (vel.x > 0)
+            {
+                vel.x -= Deceleration * Time.deltaTime;
+                if (vel.x < 0)
+                {
+                    vel.x = 0;
+                }
+            }
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            vel.y = 10;
+            vel.y = JumpVel;
         }
         rb.velocity = vel;
     }
